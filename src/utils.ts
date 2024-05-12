82b4ce10197f017aaceadf37ad6825cb7ec75f25
SkwalExe <skwal@skwal.net>
2024-05-12 15:24:26 +0200

    separate code in utils.ts

---
+++ src/utils.ts
@@ -0,0 +1,15 @@
+export const GetRandomInt = (min: number, max: number): number => {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    const RandomInt = Math.floor(Math.random() * (max - min + 1)) + min
+    return RandomInt
+}
+
+// Shorthand for document.querySelectorAll<HTMLElement>("selector")
+export const $$ = (selector: string) => document.querySelectorAll<HTMLElement>(selector)
+
+export const playSound = (path: string) => {
+    new AudioContext()
+    const audio = new Audio(path)
+    audio.play()
+}
